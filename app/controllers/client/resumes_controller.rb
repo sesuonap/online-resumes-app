dff2d974964f61996ce4b6891012b0b6c4f9a27c
conradskittles <conradszklarz@gmail.com>
2018-07-05 21:23:44 -0500

    added index and show

--- app/controllers/client/resumes_controller.rb
+++ app/controllers/client/resumes_controller.rb
@@ -25,7 +25,51 @@ class Client::ResumesController < ApplicationController
 
   
   def show 
-    student_id = params[:id]
+    @resume = {
+                id: 1,
+                first_name: "trey",
+                last_name: "conrad",
+                email: "trey@gmail.com",
+                phone_number: "5555555",
+                bio: "im awesome",
+                linkein_url: "linkinin",
+                personal_blog_url: "dfgf",
+                github_url: "dfgdf",
+                photo_url: "fdhdf",
+                capstone_name: "dfgdf",
+                capstone_description: "dfgdfg",
+                capstone_url: "dfghfd",
+                experiences: [
+                              {
+                              id: 1,
+                              student_id: 1,
+                              start_date: "dfgfdg",
+                              end_date: "fdg",
+                              job_title: "gsdg",
+                              company_name: "xcfgfd",
+                              details: "fdgfd"
+                              }
+                              ],
+                educations: [
+                            {
+                              id: 1,
+                              student_id: 1,
+                              start_date: "fghfg",
+                              end_date: "dfg",
+                              degree: "dfgdf",
+                              university_name: "cfhfdg",
+                              details: "dfhdfg"
+                              }
+                            ],
+                skills: [
+                         {
+                          id: 1,    
+                          student_id: 1,
+                          skill_name: "trey"
+                          }
+                        ]
+              }
+    # @resume = params[:id]
     # response = Unirest.get("http://localhost:3000/api/#{student_id}")
     # @student = response.body
     render 'show.html.erb'
