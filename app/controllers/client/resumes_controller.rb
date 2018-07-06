a556158824afef27406eae2bbf1e411d26a820d0
treybezy <reginaldbarnesiii@gmail.com>
2018-07-05 20:46:02 -0500

    store

--- app/controllers/client/resumes_controller.rb
+++ app/controllers/client/resumes_controller.rb
@@ -1,20 +1,33 @@
 class Client::ResumesController < ApplicationController
 
   def index 
-    client_params = {
-                    student: params[:student]
-                    }
+    # client_params = {
+    #                 student: params[:student]
+    #                 }
 
-        response = Unirest.get("http://localhost:3000/api/")
-        @student = response.body
-        render 'index.html.erb'
+    #     response = Unirest.get("http://localhost:3000/api/")
+    #     @student = response.body
+
+    @resumes = [
+                  {
+                    id: 1,
+                    first_name: "bob",
+                    last_name: "jones",
+                  }, 
+                  {
+                    id: 2, 
+                    first_name: "trey", 
+                    last_name: "conrad"
+                  }
+                ]
+    render 'index.html.erb'
   end 
 
   
   def show 
     student_id = params[:id]
-    response = Unirest.get("http://localhost:3000/api/#{student_id}")
-    @student = response.body
+    # response = Unirest.get("http://localhost:3000/api/#{student_id}")
+    # @student = response.body
     render 'show.html.erb'
   end 
 
